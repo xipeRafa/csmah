6f3c45d086117d4459f07a2627b48ca27966d206
fer <fer@fers-iMac.local>
2024-08-27 14:13:08 -0700

    lpl

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from "./components/navbar/Navbar";
+import './css/App.css'
+
+function App() {
+
+  return (
+    <div className="containerApp">
+        <Navbar />
+        <p>koko</p>
+    </div>
+  )
+}
+
+export default App
