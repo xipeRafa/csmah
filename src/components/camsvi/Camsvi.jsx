1d8ec607d3405c5060819c1823269062118e5918
fer <fer@fers-iMac.local>
2024-09-09 16:12:38 -0700

    lplp

--- src/components/camsvi/Camsvi.jsx
+++ src/components/camsvi/Camsvi.jsx
@@ -1,24 +1,19 @@
-import './camsvi.css'
+import "./camsvi.css";
 
+import CAMSVI from "../../imgs/camsvi.png";
 
+import CAMSVI2 from "../../imgs/2.png";
 
 export default function Camsvi() {
-
-
-	  return (
-
-
-
-            <div>
-                <p>Camsvi</p>
-                 <p>Camsvi</p>
-                  <p>Camsvi</p>
-                   <p>Camsvi</p>
-                    <p>Camsvi</p>
-                     <p>Camsvi</p>
-                      <p>Camsvi</p>
-                       <p>Camsvi</p>
-
+    return (
+        <div>
+            <div className="imgContainer">
+                <img src={CAMSVI} />
             </div>
-	  	)
+
+            <div className="imgContainer2">
+                <img className="logo1" src={CAMSVI2} />
+            </div>
+        </div>
+    );
 }
