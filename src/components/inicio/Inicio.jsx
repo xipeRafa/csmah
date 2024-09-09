6d0cc4afed37329546f57f2599be4c486c956c63
fer <fer@fers-iMac.local>
2024-09-09 16:48:47 -0700

    lplp

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -22,17 +22,17 @@ export default function Inicio() {
 
                <h2>SABATINAS EN HERMOSILLO</h2>
 
-               <span><p>Santa Eduwiges</p> Perimetral Oeste y Tabasco Norte, Col. Modelo           <img  src={SE} />   <hr /></span> 
+               <span><p>Santa Eduwiges</p> Perimetral Oeste y Tabasco Norte<br /> Col. Modelo           <img  src={SE} />   <hr /></span> 
 
-               <span><p>Sagrado Corazón de Jesús</p> Rayón y Fronteras, Col. 5 de Mayo        <img  src={SCJ} />   <hr /></span> 
+               <span><p>Sagrado Corazón de Jesús</p> Rayón y Fronteras<br /> Col. 5 de Mayo        <img  src={SCJ} />   <hr /></span> 
 
-               <span><p>San Juan Capistrano</p> Calzada San Bernardino Entre Colosio y Navarrete # 52, Col. Seminario      <img  src={SJC} />   <hr /></span>  
+               <span><p>San Juan Capistrano</p> Calzada San Bernardino Entre Colosio y Navarrete # 52<br /> Col. Seminario      <img  src={SJC} />   <hr /></span>  
 
-               <span><p>Santo Niño de Praga</p> Quinta Emilia       <img  src={SNP} />   <hr /></span>  
+               <span><p>Santo Niño de Praga</p> Col. Quinta Emilia       <img  src={SNP} />   <hr /></span>  
 
-               <span><p>Nuestro Señor Jesucristo Rey del Universo</p> Blvd. Paseo Las Quintas esq. Herminia Valencia, Col. Las Minitas    <img  src={CR} />   <hr /></span> 
+               <span><p>Nuestro Señor Jesucristo Rey del Universo</p> Blvd. Paseo Las Quintas esq. Herminia Valencia<br /> Col. Las Minitas    <img  src={CR} />   <hr /></span> 
 
-               <span><p>Santisima Trinidad</p> Blvd. Gómez Farías y Justo Sierra, Col. Periodista     <img  src={ST} />   <hr /></span>     
+               <span><p>Santisima Trinidad</p> Blvd. Gómez Farías y Justo Sierra<br /> Col. Periodista     <img  src={ST} />   <hr /></span>     
 
 
           </div>
