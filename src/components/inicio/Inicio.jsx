8b16f6316c91155908c74f18e9932c1bd9afaaed
fer <fer@fers-iMac.local>
2024-09-10 12:13:09 -0700

    koko

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -12,6 +12,9 @@ import CR from '../../imgs/cr.png'
 
 import ST from '../../imgs/st.png'
 
+    import mmsv from '../../imgs/mmsv.png'
+
+
 
 export default function Inicio() {
 
@@ -20,19 +23,28 @@ export default function Inicio() {
 
           <div className='inicio'>
 
+
                <h2>SABATINAS EN HERMOSILLO</h2>
 
-               <span><p>Santa Eduwiges</p> Perimetral Oeste y Tabasco Norte<br /> Col. Modelo           <img  src={SE} />   <hr /></span> 
 
-               <span><p>Sagrado Corazón de Jesús</p> Rayón y Fronteras<br /> Col. 5 de Mayo        <img  src={SCJ} />   <hr /></span> 
+               <div className='mmsvContainer'>
+                    <img className='mmsv'  src={mmsv} />
+                     <hr />
+               </div>
 
-               <span><p>San Juan Capistrano</p> Calzada San Bernardino Entre Colosio y Navarrete # 52<br /> Col. Seminario      <img  src={SJC} />   <hr /></span>  
+               <br />
 
-               <span><p>Santo Niño de Praga</p> Col. Quinta Emilia       <img  src={SNP} />   <hr /></span>  
+               <span><p>Santa Eduwiges</p> Perimetral Oeste y Tabasco Norte<br /> Col. Modelo, Sabados 7:00 AM<img  src={SE} />   <hr /></span> 
 
-               <span><p>Nuestro Señor Jesucristo Rey del Universo</p> Blvd. Paseo Las Quintas esq. Herminia Valencia<br /> Col. Las Minitas    <img  src={CR} />   <hr /></span> 
+               <span><p>Sagrado Corazón de Jesús</p> Rayón y Fronteras<br /> Col. 5 de Mayo, Sabados 7:00 AM        <img  src={SCJ} />   <hr /></span> 
 
-               <span><p>Santisima Trinidad</p> Blvd. Gómez Farías y Justo Sierra<br /> Col. Periodista     <img  src={ST} />   <hr /></span>     
+               <span><p>San Juan Capistrano</p> Calzada San Bernardino Entre Colosio y Navarrete # 52<br /> Col. Seminario, Sabados 7:00 AM      <img  src={SJC} />   <hr /></span>  
+
+               <span><p>Santo Niño de Praga</p> Col. Quinta Emilia, Sabados 7:00 AM       <img  src={SNP} />   <hr /></span>  
+
+               <span><p>Nuestro Señor Jesucristo Rey del Universo</p> Blvd. Paseo Las Quintas esq. Herminia Valencia<br /> Col. Las Minitas, Sabados 7:40 AM    <img  src={CR} />   <hr /></span> 
+
+               <span><p>Santisima Trinidad</p> Blvd. Gómez Farías y Justo Sierra<br /> Col. Periodista, Sabados 7:00 AM     <img  src={ST} />   <hr /></span>     
 
 
           </div>
