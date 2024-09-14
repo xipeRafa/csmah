4d2d57f0b7788400252b0c25259ec781255cc245
fer <fer@fers-iMac.local>
2024-09-13 22:19:33 -0700

    lplp

--- src/components/comedor/Comedor.jsx
+++ src/components/comedor/Comedor.jsx
@@ -12,23 +12,7 @@ export default function Comedor() {
 
 
             <iframe className='comedor' src="https://xiperafa.github.io/csma-comedor/"></iframe>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-                <p>comedor</p>
-
+          
             </div>
 	  	)
 }
